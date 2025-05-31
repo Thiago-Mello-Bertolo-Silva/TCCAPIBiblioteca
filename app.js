1f91b32b85e99987f1eae8f1d57fcff61aa1cab9
Thiago-Mello-Bertolo-Silva <241052.thiago@liceusantista.com.br>
2025-05-31 19:34:50 -0300

    cors

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 dotenv.config();
 
 import './src/models/Assossiation.js';
